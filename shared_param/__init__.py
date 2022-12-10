a5a93e22cbcaeab7b2dfa835261f1eae2e0eabe6
Omar Elnashar <oel@corelia.ai>
2022-12-10 16:23:00 +0200

    add shared parameters

---
+++ shared_param/__init__.py
@@ -0,0 +1,1 @@
+from .shared_param import model_path,tokenizer_path,input_dim,input_length,output_dim
---
+++ shared_param/shared_param.py
@@ -0,0 +1,5 @@
+tokenizer_path = "./final_models/tokenizers/tokenizer.pickle"
+model_path = "final_models/try.h5"
+input_dim=30000 
+output_dim=30
+input_length=5
